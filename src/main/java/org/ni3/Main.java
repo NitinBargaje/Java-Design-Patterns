88106a3ca0b19a17665774cdd4e83b48417d7017
NitinBargaje <nitinbargaje7@gmail.com>
2024-02-14 17:53:14 +0530

    Initial commit

---
+++ src/main/java/org/ni3/Main.java
@@ -0,0 +1,10 @@
+package org.ni3;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = new TreeMap<>();
+    }
+}
